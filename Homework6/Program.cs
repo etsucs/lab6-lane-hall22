55b673cb2c273c565b36a745e6bf5f902a6ec4b1
lane-hall22 <99291690+lane-hall22@users.noreply.github.com>
2022-03-28 03:56:09 -0400

    Add files via upload

---
+++ Homework6/Program.cs
@@ -0,0 +1,76 @@
+﻿
+//SHOW CHARACTER
+
+static void ShowCharacter(string? word, int letter)
+{
+    Console.WriteLine(word?[letter - 1]);
+}
+
+ShowCharacter("dog", 2);
+ShowCharacter("barbeque", 7);
+ShowCharacter("potato", 3);
+
+
+
+
+
+//RETAIL PRICE
+
+
+ Console.Write("Please enter your item's wholesale cost. ");
+   decimal whole = Convert.ToDecimal(Console.ReadLine());
+    Console.Write("Please enter your item's markup percentage. ");
+   decimal mark = Convert.ToDecimal(Console.ReadLine());
+
+CalculateRetail(whole, mark);
+
+static void CalculateRetail(decimal wholesale, decimal markup)
+{
+    decimal markupVariable = Convert.ToDecimal("0.01");
+    decimal markupValue = markup * markupVariable;
+    decimal retail = (wholesale * markupValue) + wholesale;
+
+
+    Console.WriteLine($"The retail price is ${retail}!");
+
+}
+
+
+
+
+
+//TEMP TABLE
+
+static decimal Celsius(decimal fahrenheit)
+{
+    decimal calcVar = Convert.ToDecimal(0.56);
+    decimal celsius = calcVar * (fahrenheit - 32);
+    return celsius;
+
+}
+
+decimal fahren = 80;
+
+for (int i = 0; i < 21; i++)
+{
+    Console.WriteLine($"{fahren} degrees F = {Celsius(fahren++)} degrees C!");
+}
+
+
+
+
+
+//PRIME NUMBERS
+
+/*
+Console.WriteLine("Enter a number to see if it is prime or not! ");
+int primeTest = Convert.ToInt32(Console.ReadLine());
+
+static void IsPrime(int prime)
+{
+    if(prime == 1 )
+}
+
+//so confused
+
+*/
